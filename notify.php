e78cd1b2f9e4b94ba2a701b1569cebe06fbbb3fd
louaolin <louaolin@163.com>
2022-05-24 18:31:19 +0800

    添加 回调文件

---
+++ notify.php
@@ -0,0 +1,162 @@
+<?php
+header('Content-type:text/html; Charset=utf-8');
+
+//支付宝公钥
+$alipayPublicKey='';
+
+$aliPay = new AlipayService($alipayPublicKey);
+
+//验证签名
+$result = $aliPay->rsaCheck($_POST,$_POST['sign_type']);
+
+/* 实际验证过程建议商户添加以下校验。
+1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
+2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额），
+3、校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方（有的时候，一个商户可能有多个seller_id/seller_email）
+4、验证app_id是否为该商户本身。
+*/
+
+if($result===true){//验证成功
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //请在这里加上商户的业务逻辑程序代
+
+
+    //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+
+    //获取支付宝的通知返回参数，可参考技术文档中服务器异步通知参数列表
+
+    //商户订单号
+
+    $out_trade_no = $_POST['out_trade_no'];
+
+    //支付宝交易号
+
+    $trade_no = $_POST['trade_no'];
+
+    //交易状态
+    $trade_status = $_POST['trade_status'];
+
+
+    if($_POST['trade_status'] == 'TRADE_FINISHED') {
+
+        //判断该笔订单是否在商户网站中已经做过处理
+        //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+        //请务必判断请求时的total_amount与通知时获取的total_fee为一致的
+        //如果有做过处理，不执行商户的业务程序
+
+        //注意：
+        //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+    }
+    else if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
+        //判断该笔订单是否在商户网站中已经做过处理
+        //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+        //请务必判断请求时的total_amount与通知时获取的total_fee为一致的
+        //如果有做过处理，不执行商户的业务程序
+        //注意：
+        //付款完成后，支付宝系统发送该交易状态通知
+    }
+    //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+    echo "success";	//请不要修改或删除
+}else {
+    //验证失败
+    echo "fail";exit();
+
+}
+echo 'error';exit();
+
+
+class AlipayService
+{
+    //支付宝公钥
+    protected $alipayPublicKey;
+    protected $charset;
+
+    public function __construct($alipayPublicKey)
+    {
+        $this->charset = 'utf8';
+        $this->alipayPublicKey=$alipayPublicKey;
+    }
+
+    /**
+     *  验证签名
+     **/
+    public function rsaCheck($params) {
+        $sign = $params['sign'];
+        $signType = $params['sign_type'];
+        unset($params['sign_type']);
+        unset($params['sign']);
+        return $this->verify($this->getSignContent($params), $sign, $signType);
+    }
+
+    function verify($data, $sign, $signType = 'RSA') {
+        $pubKey= $this->alipayPublicKey;
+        $res = "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($pubKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
+        ($res) or die('支付宝RSA公钥错误。请检查公钥文件格式是否正确');
+
+        //调用openssl内置方法验签，返回bool值
+        if ("RSA2" == $signType) {
+            $result = (bool)openssl_verify($data, base64_decode($sign), $res, version_compare(PHP_VERSION,'5.4.0', '<') ? SHA256 : OPENSSL_ALGO_SHA256);
+        } else {
+            $result = (bool)openssl_verify($data, base64_decode($sign), $res);
+        }
+//        if(!$this->checkEmpty($this->alipayPublicKey)) {
+//            //释放资源
+//            openssl_free_key($res);
+//        }
+        return $result;
+    }
+
+    /**
+     * 校验$value是否非空
+     *  if not set ,return true;
+     *    if is null , return true;
+     **/
+    protected function checkEmpty($value) {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+        return false;
+    }
+
+    public function getSignContent($params) {
+        ksort($params);
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === $this->checkEmpty($v) && "@" != substr($v, 0, 1)) {
+                // 转换成目标字符集
+                $v = $this->characet($v, $this->charset);
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+
+    /**
+     * 转换字符集编码
+     * @param $data
+     * @param $targetCharset
+     * @return string
+     */
+    function characet($data, $targetCharset) {
+        if (!empty($data)) {
+            $fileType = $this->charset;
+            if (strcasecmp($fileType, $targetCharset) != 0) {
+                $data = mb_convert_encoding($data, $targetCharset, $fileType);
+                //$data = iconv($fileType, $targetCharset.'//IGNORE', $data);
+            }
+        }
+        return $data;
+    }
+}
---
+++ return.php
@@ -0,0 +1,140 @@
+<?php
+header('Content-type:text/html; Charset=utf-8');
+
+//支付宝公钥
+$alipayPublicKey='';
+
+$aliPay = new AlipayService($alipayPublicKey);
+
+//验证签名
+$result = $aliPay->rsaCheck($_POST,$_POST['sign_type']);
+
+/* 实际验证过程建议商户添加以下校验。
+1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
+2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额），
+3、校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方（有的时候，一个商户可能有多个seller_id/seller_email）
+4、验证app_id是否为该商户本身。
+*/
+
+if($result===true){//验证成功
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //请在这里加上商户的业务逻辑程序代码
+
+    //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+    //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表
+    //同步回调一般不处理业务逻辑，显示一个付款成功的页面，或者跳转到用户的财务记录页面即可。
+
+    //商户订单号
+    $out_trade_no = htmlspecialchars($_GET['out_trade_no']);
+
+    //支付宝交易号
+    $trade_no = htmlspecialchars($_GET['trade_no']);
+
+    echo "<h1>付款成功</h1> <br />支付宝交易号：".$trade_no;
+
+    //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+} else {
+    //验证失败
+    echo "验证失败";exit();
+}
+
+echo '不合法的请求';exit();
+
+class AlipayService
+{
+    //支付宝公钥
+    protected $alipayPublicKey;
+    protected $charset;
+
+    public function __construct($alipayPublicKey)
+    {
+        $this->charset = 'utf8';
+        $this->alipayPublicKey=$alipayPublicKey;
+    }
+
+    /**
+     *  验证签名
+     **/
+    public function rsaCheck($params) {
+        $sign = $params['sign'];
+        $signType = $params['sign_type'];
+        unset($params['sign_type']);
+        unset($params['sign']);
+        return $this->verify($this->getSignContent($params), $sign, $signType);
+    }
+
+    function verify($data, $sign, $signType = 'RSA') {
+        $pubKey= $this->alipayPublicKey;
+        $res = "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($pubKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
+        ($res) or die('支付宝RSA公钥错误。请检查公钥文件格式是否正确');
+
+        //调用openssl内置方法验签，返回bool值
+        if ("RSA2" == $signType) {
+            $result = (bool)openssl_verify($data, base64_decode($sign), $res, version_compare(PHP_VERSION,'5.4.0', '<') ? SHA256 : OPENSSL_ALGO_SHA256);
+        } else {
+            $result = (bool)openssl_verify($data, base64_decode($sign), $res);
+        }
+//        if(!$this->checkEmpty($this->alipayPublicKey)) {
+//            //释放资源
+//            openssl_free_key($res);
+//        }
+        return $result;
+    }
+
+    /**
+     * 校验$value是否非空
+     *  if not set ,return true;
+     *    if is null , return true;
+     **/
+    protected function checkEmpty($value) {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+
+        return false;
+    }
+
+    public function getSignContent($params) {
+        ksort($params);
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === $this->checkEmpty($v) && "@" != substr($v, 0, 1)) {
+                // 转换成目标字符集
+                $v = $this->characet($v, $this->charset);
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+
+    /**
+     * 转换字符集编码
+     * @param $data
+     * @param $targetCharset
+     * @return string
+     */
+    function characet($data, $targetCharset) {
+        if (!empty($data)) {
+            $fileType = $this->charset;
+            if (strcasecmp($fileType, $targetCharset) != 0) {
+                $data = mb_convert_encoding($data, $targetCharset, $fileType);
+                //$data = iconv($fileType, $targetCharset.'//IGNORE', $data);
+            }
+        }
+        return $data;
+    }
+}
